41a8f241254fcaa30788db94f57fa154a6aa008c
Josh Starcher <josh.starcher@gmail.com>
2012-05-23 13:37:04 -0400

    use preferred name

--- app/models/person.rb
+++ app/models/person.rb
@@ -11,4 +11,8 @@ class Person < ActiveRecord::Base
     Person.connection.select_value("select `value` from staffsite_staffsitepref pref join `staffsite_staffsiteprofile` prof on pref.fk_StaffsiteProfile = prof.`StaffSiteProfileID` join ministry_person p on p.accountNo = prof.accountNo where p.personID = #{id} AND pref.name = 'CURRENT_BALANCE'").to_f
   end
 
+  def first_name
+    preferredName.present? ? preferredName : firstName
+  end
+
 end
