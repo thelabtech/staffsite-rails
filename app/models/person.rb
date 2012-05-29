5fd6dab9d3025d460c32ba04625fce153bde6780
Justin Sabelko <justin.sabelko@uscm.org>
2012-05-29 14:22:44 -0500

    Fetch correct account balance field

--- app/models/person.rb
+++ app/models/person.rb
@@ -8,7 +8,8 @@ class Person < ActiveRecord::Base
   has_many :teams, through: :team_memberships
 
   def account_balance
-    Person.connection.select_value("select `value` from staffsite_staffsitepref pref join `staffsite_staffsiteprofile` prof on pref.fk_StaffsiteProfile = prof.`StaffSiteProfileID` join ministry_person p on p.accountNo = prof.accountNo where p.personID = #{id} AND pref.name = 'CURRENT_BALANCE'").to_f
+    #TODO: Use classes and arel (at least through user), not manual query
+    Person.connection.select_value("select `value` from infobase_bookmarks pref join `simplesecuritymanager_user` user on pref.user_id = user.`UserID` join ministry_person p on p.fk_ssmUserId = user.UserID where p.personID = #{id} AND pref.name = 'balance'").to_f
   end
 
   def first_name
