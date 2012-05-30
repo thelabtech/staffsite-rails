ced7074b3dbc66127f7768598296dffd3d9d9f03
Josh Starcher <josh.starcher@gmail.com>
2012-05-30 11:47:26 -0400

    let non-staff in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,8 @@
 class ApplicationController < ActionController::Base
   before_filter CASClient::Frameworks::Rails::Filter
-  before_filter :ensure_staff
   protect_from_forgery
 
   protected
-  def ensure_staff
-    unless current_person && current_person.isStaff?
-      redirect_to '/no_access'
-    end
-  end
 
   def current_user
     return @current_user if @current_user
